8f048eeb98770412f48bc98056fe5a2c2431bd79
Darkerego <darkerego@users.noreply.github.com>
2015-12-04 20:53:10 -0500

    Update auto1liner.py
    Now writes to file and actually 'auto one-lines' it for you.

--- auto1liner.py
+++ auto1liner.py
@@ -1,30 +1,55 @@
 #!/usr/bin/python env
-
+import sys,os
 
 print("""
-
-─┼─┼─┬  ╔═╗╦ ╦╔═╗╦  ╦  ╔═╗╔═╗╔╦╗╔═╗          
+ ┼─┼─┬  ╔═╗╦ ╦╔═╗╦  ╦  ╔═╗╔═╗╔╦╗╔═╗          
 ─┼─┼─│  ╚═╗╠═╣║╣ ║  ║  ║  ║ ║ ║║║╣           
      o  ╚═╝╩ ╩╚═╝╩═╝╩═╝╚═╝╚═╝═╩╝╚═╝          
         ┌─┐┬ ┬┌┬┐┌─┐   ┌─┐┌┐┌┌─┐┬  ┬┌┐┌┌─┐┬─┐
         ├─┤│ │ │ │ │───│ ││││├┤ │  ││││├┤ ├┬┘
         ┴ ┴└─┘ ┴ └─┘   └─┘┘└┘└─┘┴─┘┴┘└┘└─┘┴└─
           Ever need to write a script and
-            you dont have a text editor
-            (for whatever reason...)?
-
-
+            you dont have a text editor?
+             A python script to write 
+              shell scripts using
+               nothing but the
+                echo command!
+                 
+            
 """)
 
 
-a = ("OK", "ok")
+a = ("_EOS_", "_eos_")
 out = str(input("Enter the name of script to write :"))
-print("Enter commands, one line at a time. (Enter OK or ok when done).")
-print("touch %s ;" % out)
-run = "0"
-while run == "0":
-    cmd = str(input(": "))
-    if cmd in set (a):
-        run = "1"
-    else:
-       print("echo " + '"' + (cmd)+ '" >> ' + (out) + ";")
+print("When finsished, type '_EOS_' to close.")
+intext = open('oneliner-out','w+').readline()
+print(intext)
+
+
+def write(cmd):
+    #cmd = ("echo " + '"' + (cmd)+ '" >> ' + (out) + ";")
+    d = open(out, 'a')
+    d.write(cmd)
+    d.close()
+    n = open(out, 'r').readline()
+    print(n)
+
+# first command should not append
+
+def main():
+    run = "0"
+    cmd = str(input("Enter a command: "))
+    cmd = ("echo " + '"' + (cmd)+ '" > ' + (out) + ";")
+    write(cmd)
+
+    while run == "0":
+        cmd = str(input("Enter a command: "))
+        if cmd in set (a):
+            run = "1"
+            t = open(out, 'r').readline()
+        
+        else:
+            cmd = ("echo " + '"' + (cmd)+ '" >> ' + (out) + ";")
+            write(cmd)
+       
+main()
